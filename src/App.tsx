5e7cb19967965582454d2029181d5ab74fd72fca
이홍준 <zunza96@naver.com>
2023-02-12 22:50:02 +0900

    리스트 기본기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import {
   RecoilRoot,
   atom,
@@ -17,10 +17,13 @@ function App() {
 }
 
 function TodoList() {
-  const todoList = useRecoilValue(todoListState);
+  const todoList = useRecoilValue(filteredTodoListState);
   return (
     <>
+      <TodoListStats />
+      <TodoListFilters />
       <TodoItemCreator />
+
       {todoList.map((todoItem: Todo) => (
         <TodoItem key={todoItem.id} item={todoItem} />
       ))}
@@ -111,5 +114,74 @@ function replaceItemAtIndex(arr: Todo[], index: number, newValue: Todo) {
 }
 function removeItemAtIndex(arr: Todo[], index: number) {
   return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
+const todoListFilterState = atom({
+  key: "todoListFilterState",
+  default: "Show All",
+});
+const filteredTodoListState = selector({
+  key: "filteredTodoListState",
+  get: ({ get }) => {
+    const filter = get(todoListFilterState);
+    const list = get(todoListState);
+    switch (filter) {
+      case "Show Completed":
+        return list.filter((item) => item.isComplete);
+      case "Show Uncompleted":
+        return list.filter((item) => !item.isComplete);
+      default:
+        return list;
+    }
+  },
+});
+
+function TodoListFilters() {
+  const [filter, setFilter] = useRecoilState(todoListFilterState);
+  const updateFilter = (event: ChangeEvent<HTMLSelectElement>) => {
+    setFilter(event.target.value);
+  };
+  return (
+    <>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        <option value="Show All">All</option>
+        <option value="Show Completed">Completed</option>
+        <option value="Show Uncompleted">Uncompleted</option>
+      </select>
+    </>
+  );
+}
+const todoListStatsState = selector({
+  key: "todoListStatsState",
+  get: ({ get }) => {
+    const todoList = get(todoListState);
+    const totalNum = todoList.length;
+    const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
+    const totalUncompletedNum = totalNum - totalCompletedNum;
+    const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
+    return {
+      totalNum,
+      totalCompletedNum,
+      totalUncompletedNum,
+      percentCompleted,
+    };
+  },
+});
+function TodoListStats() {
+  const { totalNum, totalCompletedNum, totalUncompletedNum, percentCompleted } =
+    useRecoilValue(todoListStatsState);
+
+  const formattedPercentCompleted = Math.round(percentCompleted * 100);
+
+  return (
+    <>
+      <ul>
+        <li>Total items: {totalNum}</li>
+        <li>Items completed: {totalCompletedNum}</li>
+        <li>Items not completed: {totalUncompletedNum}</li>
+        <li>Percent completed: {formattedPercentCompleted}</li>
+      </ul>
+    </>
+  );
 }
 export default App;
