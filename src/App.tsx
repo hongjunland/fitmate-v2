732b8512994a28e437b527577b11a4358f8c74e6
이홍준 <zunza96@naver.com>
2023-02-12 21:03:15 +0900

    list 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <RecoilRoot>
+      <TodoList />
+    </RecoilRoot>
+  );
+}
+
+function TodoList() {
+  const todoList = useRecoilValue(todoListState);
+  return (
+    <>
+      <TodoItemCreator />
+      {todoList.map((todoItem: Todo) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
+    </>
+  );
+}
+const todoListState = atom<Todo[]>({
+  key: "todoListState",
+  default: [],
+});
+interface Todo {
+  id: number;
+  text: string;
+  isComplete: boolean;
+}
+function TodoItemCreator() {
+  const [inputValue, setInputValue] = useState("");
+  const setTodoList = useSetRecoilState<Todo[]>(todoListState);
+
+  const addItem = () => {
+    setTodoList((oldTodoList) => [
+      ...oldTodoList,
+      {
+        id: getId(),
+        text: inputValue,
+        isComplete: false,
+      },
+    ]);
+    setInputValue("");
+  };
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={inputValue} onChange={onChange} />
+      <button onClick={addItem}>Add</button>
     </div>
   );
 }
 
+// 고유한 Id 생성을 위한 유틸리티
+let id = 0;
+function getId() {
+  return id++;
+}
+interface TodoItemProps {
+  item: Todo;
+}
+function TodoItem({ item }: TodoItemProps) {
+  const [todoList, setTodoList] = useRecoilState(todoListState);
+  const index = todoList.findIndex((listItem) => listItem === item);
+  const editItemText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: event.target.value,
+    });
+    setTodoList(newList);
+  };
+  const toggleItemCompletion = () => {
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    });
+    setTodoList(newList);
+  };
+  const deleteItem = () => {
+    const newList = removeItemAtIndex(todoList, index);
+    setTodoList(newList);
+  };
+  return (
+    <div>
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>X</button>
+    </div>
+  );
+}
+
+function replaceItemAtIndex(arr: Todo[], index: number, newValue: Todo) {
+  return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
+}
+function removeItemAtIndex(arr: Todo[], index: number) {
+  return [...arr.slice(0, index), ...arr.slice(index + 1)];
+}
 export default App;
