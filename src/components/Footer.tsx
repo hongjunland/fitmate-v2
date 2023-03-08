957f4c597f82a1b4138a33241288f04050c734c7
이홍준 <zunza96@naver.com>
2023-03-08 18:45:48 +0900

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,27 @@
 import styled from "@emotion/styled";
-import { Grid, Icon, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import HangerImg from 'assets/hanger.png';
 export default function Footer() {
   return (
     <Container>
       <Grid container>
-        <Grid item md={3}>
-          <div>
-            <FooterImg src="assets/hanger.png" alt="hanger" />
-          </div>
+        <Grid item md={3} justifyContent={"center"} display={"flex"}>
+          <FooterImg src={HangerImg} alt="hanger" />
         </Grid>
         <Grid item md={7}>
           <CopyrightBox>
             <Typography variant={"h5"} color={"black"}>
-              ⓒFitmate{" "}
+              ⓒFitmate
             </Typography>
-            <p>
+            <Typography variant={"inherit"}>
               본 사이트의 아이디어, 디자인을 포함한 콘텐츠는 저작권법의 보호를
               받는지는 잘 모르겠습니다.
-            </p>
-            <p>아무튼 퍼가지 마세요..</p>
+            </Typography>
+            <Typography variant={"inherit"}>아무튼 퍼가지 마세요..</Typography>
           </CopyrightBox>
         </Grid>
         <Grid item md={2} alignItems={"center"} display={"flex"}>
@@ -41,6 +40,8 @@ export default function Footer() {
 const Container = styled.footer`
   width: 100%;
   bottom: 0px;
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
   position: absolute;
   background-color: #cbc7cf;
   align-content: center;
